347f576bd79ae95b3bde302ee8c277983f1dd83d
Luis Felipe Teles <lfelipet01@gmail.com>
2025-05-01 17:05:20 -0300

    hash code

---
+++ equalshashcode/src/main/java/com/teles/Main.java
@@ -0,0 +1,25 @@
+package com.teles;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        List<Carro> listaCarros = new ArrayList<>();
+
+        listaCarros.add(new Carro("Ford"));
+        listaCarros.add(new Carro("Chevrolet"));
+        listaCarros.add(new Carro("Volsksvagen"));
+
+        System.out.println(listaCarros.contains(new Carro("Ford")));
+        System.out.println(new Carro("Ford").hashCode());
+
+        
+
+
+
+        
+    }
+}
